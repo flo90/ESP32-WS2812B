35c7e264cfb8576cab07556c9b17f9e4eec815de
Florian Menne <flo90dev@gmail.com>
2018-01-21 11:46:03 +0100

    Moved config parameters from component.mk to WS2812B.c;

--- WS2812B.c
+++ WS2812B.c
@@ -17,16 +17,16 @@
 
 #include "stdio.h"
 
-#ifndef WS2812B_INVERTED
-#define WS2812B_INVERTED 0
+#ifndef CONFIG_WS2812B_INVERTED
+#define CONFIG_WS2812B_INVERTED 0
 #endif
 
-#ifndef WS2812B_USE_PL9823
-#define WS2812B_USE_PL9823 0
+#ifndef CONFIG_WS2812B_USE_PL9823
+#define CONFIG_WS2812B_USE_PL9823 0
 #endif
 
-#if WS2812B_INVERTED == 0
-#if WS2812B_USE_PL9823 == 0
+#if CONFIG_WS2812B_INVERTED == 0
+#if CONFIG_WS2812B_USE_PL9823 == 0
 static const rmt_item32_t wsLogicZero = {.level0 = 1, .duration0 = 32, .level1 = 0, .duration1 = 68};
 static const rmt_item32_t wsLogicOne = {.level0 = 1, .duration0 = 64, .level1 = 0, .duration1 = 36};
 #else
@@ -35,7 +35,7 @@ static const rmt_item32_t wsLogicOne = {.level0 = 1, .duration0 = 109, .level1 =
 #endif
 
 #else
-#if WS2812B_USE_PL9823 == 0
+#if CONFIG_WS2812B_USE_PL9823 == 0
 static const rmt_item32_t wsLogicZero = {.level0 = 0, .duration0 = 32, .level1 = 1, .duration1 = 68};
 static const rmt_item32_t wsLogicOne = {.level0 = 0, .duration0 = 64, .level1 = 1, .duration1 = 36};
 #else
@@ -75,7 +75,7 @@ void WS2812B_init(rmt_channel_t chan, gpio_num_t gpio, unsigned int psize)
 	rmt_tx.mem_block_num = 1;
 	rmt_tx.clk_div = 1;
 	rmt_tx.tx_config.idle_output_en = 1;
-#if WS2812B_INVERTED == 1
+#if CONFIG_WS2812B_INVERTED == 1
 	rmt_tx.tx_config.idle_level = RMT_IDLE_LEVEL_HIGH;
 #endif
 
@@ -92,7 +92,7 @@ void WS2812B_setLeds(wsRGB_t* data, unsigned int size)
 		{
 			if(j < 8)
 			{
-#if WS2812B_USE_PL9823 == 0
+#if CONFIG_WS2812B_USE_PL9823 == 0
 				if(data[i].g & (1<<(7-j))) items[itemCnt++] = wsLogicOne;
 #else
 				if(data[i].r & (1<<(7-j))) items[itemCnt++] = wsLogicOne;
@@ -102,7 +102,7 @@ void WS2812B_setLeds(wsRGB_t* data, unsigned int size)
 
 			else if (j < 16)
 			{
-#if WS2812B_USE_PL9823 == 0
+#if CONFIG_WS2812B_USE_PL9823 == 0
 				if(data[i].r & (1<<(7 - (j%8) ))) items[itemCnt++] = wsLogicOne;
 #else
 				if(data[i].g & (1<<(7 - (j%8) ))) items[itemCnt++] = wsLogicOne;
