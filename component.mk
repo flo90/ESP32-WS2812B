6431de6ebfb5b8c3617d8d62a166eb4d86ad85ca
Florian Menne <florianmenne@t-online.de>
2018-01-21 11:46:03 +0100

    Moved config parameters from component.mk to WS2812B.c;

--- component.mk
+++ component.mk
@@ -6,13 +6,3 @@
 # in the build directory. This behaviour is entirely configurable,
 # please read the ESP-IDF documents if you need to do this.
 #
-
-ifdef CONFIG_WS2812B_INVERTED
-CFLAGS +=-DWS2812B_INVERTED=1
-endif
-
-ifdef  CONFIG_WS2812B_USE_PL9823
-CFLAGS +=-DWS2812B_USE_PL9823=1
-endif
-
-
